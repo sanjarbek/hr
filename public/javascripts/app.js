1820ab6b41d3ab5f5e4279c386c0588e37e28006
Sanzharbek Amatov <asanjarbek@gmail.com>
2014-09-06 08:33:36 +0600

    Refactoring angularjs code.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,26 +11,7 @@ angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
         .state('employees.create', {
             url: '/create',
             templateUrl: '/employees/create',
-            controller: function ($scope, EmployeeService) {
-                $scope.saveEmployee = function () {
-                    var data = {
-                        id: 0,
-                        surname: $scope.newEmployeeForm.surname,
-                        firstname: $scope.newEmployeeForm.firstname,
-                        lastname: $scope.newEmployeeForm.lastname,
-                        birthday: $scope.newEmployeeForm.birthday,
-                        citizenship: $scope.newEmployeeForm.citizenship,
-                        insurance_number: $scope.newEmployeeForm.insurance_number,
-                        tax_number: $scope.newEmployeeForm.tax_number,
-                        home_phone: $scope.newEmployeeForm.home_phone,
-                        mobile_phone: $scope.newEmployeeForm.mobile_phone,
-                        email: $scope.newEmployeeForm.email
-                    };
-
-                    EmployeeService.save(data);
-                    $scope.newEmployeeForm = {};
-                }
-            }
+            controller: 'EmployeeController'
         })
         .state('employees.list', {
             url: '/list',
@@ -40,19 +21,7 @@ angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
                     return EmployeeService.list();
                 }
             },
-            controller: function ($scope, ngTableParams, employeesData) {
-                $scope.employees = employeesData;
-
-                $scope.employeeTableParams = new ngTableParams({
-                    page: 1,            // show first page
-                    count: 10           // count per page
-                }, {
-                    total: $scope.employees.length, // length of data
-                    getData: function ($defer, params) {
-                        $defer.resolve($scope.employees.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-                    }
-                });
-            }
+            controller: 'EmployeeController'
         })
         .state('employees.detail', {
             url: '/{employeeId:[0-9]{1,6}}',
--- public/javascripts/controllers/EmployeeController.js
+++ public/javascripts/controllers/EmployeeController.js
@@ -1,12 +1,34 @@
-angular.module('app').controller('EmployeeController', function ($scope, EmployeeService, RelationshipService) {
+angular.module('app').controller('EmployeeController', function ($scope, EmployeeService, ngTableParams, employeesData) {
 
-    $scope.delete = function (id) {
-        EmployeeService.delete(id);
-        if ($scope.newEmployee.id == id) $scope.newEmployee = {};
+    $scope.saveEmployee = function () {
+        var data = {
+            id: 0,
+            surname: $scope.newEmployeeForm.surname,
+            firstname: $scope.newEmployeeForm.firstname,
+            lastname: $scope.newEmployeeForm.lastname,
+            birthday: $scope.newEmployeeForm.birthday,
+            citizenship: $scope.newEmployeeForm.citizenship,
+            insurance_number: $scope.newEmployeeForm.insurance_number,
+            tax_number: $scope.newEmployeeForm.tax_number,
+            home_phone: $scope.newEmployeeForm.home_phone,
+            mobile_phone: $scope.newEmployeeForm.mobile_phone,
+            email: $scope.newEmployeeForm.email
+        };
+
+        EmployeeService.save(data);
+        $scope.newEmployeeForm = {};
     }
 
-    $scope.edit = function (id) {
-        $scope.newEmployee = angular.copy(EmployeeService.get(id));
-    }
+    $scope.employees = employeesData;
+
+    $scope.employeeTableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+    }, {
+        total: $scope.employees.length, // length of data
+        getData: function ($defer, params) {
+            $defer.resolve($scope.employees.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
 
 })
