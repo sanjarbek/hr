974a219c389bcb386e3be8a102befc99da0264d8
Sanzharbek Amatov <asanjarbek@gmail.com>
2014-12-15 18:28:44 +0600

    added orders additional tables.

--- conf/evolutions/default/16.sql
+++ conf/evolutions/default/16.sql
@@ -13,9 +13,55 @@ CREATE TABLE orders (
 CREATE TABLE order_tags (
   id    TEXT UNIQUE,
   count INT
-)
+);
+
+CREATE TABLE employment_orders (
+  id                 BIGSERIAL PRIMARY KEY,
+  position_id        INT,
+  contract_type_id   INT,
+  employee_id        BIGINT,
+  salary             NUMERIC(14, 2),
+  calendar_type_id   SMALLINT,
+  trial_period_start DATE,
+  trial_period_end   DATE,
+  start_date         DATE,
+  end_date           DATE,
+  close_date         DATE,
+  created_at         TIMESTAMP NOT NULL,
+  updated_at         TIMESTAMP NOT NULL
+);
+
+CREATE TABLE dismissal_orders (
+  id                BIGSERIAL PRIMARY KEY,
+  employee_id       BIGINT    NOT NULL,
+  leaving_reason_id INT       NOT NULL,
+  comment           TEXT,
+  leaving_date      DATE,
+  created_at        TIMESTAMP NOT NULL,
+  updated_at        TIMESTAMP NOT NULL
+);
+
+CREATE TABLE positions (
+  id                  BIGSERIAL PRIMARY KEY,
+  employment_order_id BIGINT,
+  position_id         INT,
+  employee_id         BIGINT,
+  dismissal_order_id  BIGINT,
+  start_date          DATE,
+  end_date            DATE,
+  close_date          DATE,
+  created_at          TIMESTAMP NOT NULL,
+  updated_at          TIMESTAMP NOT NULL
+);
+
 
   # -- !Downs
-DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS positions CASCADE;
+DROP SEQUENCE IF EXISTS positions_id_seq;
+DROP TABLE IF EXISTS orders CASCADE;
 DROP SEQUENCE IF EXISTS orders_id_seq;
-DROP TABLE IF EXISTS order_tags;
+DROP TABLE IF EXISTS employment_orders CASCADE;
+DROP SEQUENCE IF EXISTS employment_orders_id_seq;
+DROP TABLE IF EXISTS dismissal_orders CASCADE;
+DROP SEQUENCE IF EXISTS dismissal_orders_id_seq;
+DROP TABLE IF EXISTS order_tags CASCADE;
--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -1,11 +1,4 @@
 # --- !Ups
 
-CREATE TABLE positions (
-  id SERIAL,
-  category_id INTEGER,
-  name varchar(30)
-);
 
 # -- !Downs
-DROP TABLE if exists positions;
-DROP SEQUENCE if exists positions_id_seq;
