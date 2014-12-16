33c6932952f65b51034ea3b3f3908533d075e105
Sanzharbek Amatov <asanjarbek@gmail.com>
2014-12-16 14:46:56 +0600

    Рефакторинг.

--- conf/evolutions/default/9.sql
+++ conf/evolutions/default/9.sql
@@ -6,6 +6,10 @@ CREATE TABLE contract_types (
   file_path TEXT
 );
 
+INSERT INTO contract_types (name, file_path)
+VALUES ('Штатный', 'work_contract_unlimited'),
+  ('Внештатный', 'work_contract_limited');
+
 # -- !Downs
 DROP TABLE IF EXISTS contract_types;
 DROP SEQUENCE IF EXISTS contract_types_id_seq;
